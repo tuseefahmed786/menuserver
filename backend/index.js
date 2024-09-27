45ffdf6b661e7c9deebd39799baf7de6529660da
tuseefahmed786 <tuseefahmed8880@gmail.com>
2024-09-27 22:56:09 +0500

    added multer,multer-storage-cloudinary,cloudinary in packages.json   "

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(userRouter)
 app.use(addCategory)
 app.use(itemsProducts)
 app.use(iconsRouter)
-// Start the server
+// Start the server 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
