aa32edeeed00f6e1067f28c679eeeb06615b197e
tuseefahmed786 <tuseefahmed8880@gmail.com>
2024-10-01 03:47:14 +0500

    index.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const itemsProducts = require('./routes/itemsProduct')
 const iconsRouter = require('./routes/icons')
 const fetchMenu = require('./routes/fetchMenu')
 const mongoose = require('mongoose');
-require('dotenv').config()
-const db = 'mongodb://atuseef261:emenutuseef@emenudb-shard-00-00.vdtcf.mongodb.net:27017,emenudb-shard-00-01.vdtcf.mongodb.net:27017,emenudb-shard-00-02.vdtcf.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmwgzm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=emenudb'
-// const db = process.env.MONGO_URI; // Make sure to set this in Vercel
+// require('dotenv').config()
+// const db = 'mongodb://atuseef261:emenutuseef@emenudb-shard-00-00.vdtcf.mongodb.net:27017,emenudb-shard-00-01.vdtcf.mongodb.net:27017,emenudb-shard-00-02.vdtcf.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmwgzm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=emenudb'
+const db = process.env.MONGO_URI; // Make sure to set this in Vercel
 // const local = 'mongodb://localhost:27017/myapp'
 mongoose.connect(db).then(() => {
     console.log("connnect db")
@@ -21,11 +21,11 @@ mongoose.connect(db).then(() => {
 
 })
 
-// app.use(cors({
-//     origin: 'https://emenu-sandy.vercel.app', // Replace with your Vercel app URL
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
-//     credentials: true, // Allow credentials (if needed)
-// }));
+app.use(cors({
+    origin: 'https://emenu-sandy.vercel.app', // Replace with your Vercel app URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+    credentials: true, // Allow credentials (if needed)
+}));
 
 app.use(bodyParser.json());
 app.use(cors());
