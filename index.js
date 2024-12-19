07224e5c3b7e2f435f7a835ac2cb221409c81b03
tuseefahmed786 <tuseefahmed8880@gmail.com>
2024-12-20 02:23:06 +0500

    hhe

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const isValidToken = require('./routes/isValidToken')
 const stripePayment = require('./routes/stripe')
 const mongoose = require('mongoose');
 const { handleStripeWebhook } = require('./controllers/handleStripeWebhook');
-// const db = 'mongodb://atuseef261:emenutuseef@emenudb-shard-00-00.vdtcf.mongodb.net:27017,emenudb-shard-00-01.vdtcf.mongodb.net:27017,emenudb-shard-00-02.vdtcf.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmwgzm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=emenudb'
-const db = process.env.MONGO_URI; // Make sure to set this in Vercel
+const db = 'mongodb://atuseef261:emenutuseef@emenudb-shard-00-00.vdtcf.mongodb.net:27017,emenudb-shard-00-01.vdtcf.mongodb.net:27017,emenudb-shard-00-02.vdtcf.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmwgzm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=emenudb'
+// const db = process.env.MONGO_URI; // Make sure to set this in Vercel
 
 mongoose.connect(db).then(() => {
     console.log("connect db")
@@ -28,11 +28,11 @@ mongoose.connect(db).then(() => {
 //     credentials: true, // Allow credentials if you're sending cookies or authorization headers
 // }));
 
-app.use(cors({
-    origin: 'https://emenu-sandy.vercel.app', // Replace with your Vercel app URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
-    credentials: true, // Allow credentials (if needed)
-}));
+// app.use(cors({
+//     origin: 'https://emenu-sandy.vercel.app', // Replace with your Vercel app URL
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+//     credentials: true, // Allow credentials (if needed)
+// }));
 
 app.use(cors());
 app.use(bodyParser.json());
