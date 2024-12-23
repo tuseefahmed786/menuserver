c65d7cb921e2323f51534c66ed33214c8d5d5a22
tuseefahmed786 <86749819+tuseefahmed786@users.noreply.github.com>
2024-12-23 23:57:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,18 +16,37 @@ const mongoose = require('mongoose');
 // const db = 'mongodb://atuseef261:emenutuseef@emenudb-shard-00-00.vdtcf.mongodb.net:27017,emenudb-shard-00-01.vdtcf.mongodb.net:27017,emenudb-shard-00-02.vdtcf.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmwgzm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=emenudb'
 const db = process.env.MONGO_URI; // Make sure to set this in Vercel
 
+
+const allowedOrigins = [
+    'https://qr.cloudymenu.com',
+    'https://www.qr.cloudymenu.com',
+    'https://www.cloudymenu.com',
+    'https://cloudymenu.com'
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
 mongoose.connect(db).then(() => {
     console.log("connected db")
 }).catch((error) => {
     console.log("error in db" + error)
 })
 
-
-app.use(cors({
-    origin: ['https://qr.cloudymenu.com', 'https://www.qr.cloudymenu.com',  'https://www.cloudymenu.com', 'https://cloudymenu.com'], // Allow both domains
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
-    credentials: true, // Allow credentials (if needed)
-}));
+// app.use(cors({
+//     origin: ['https://qr.cloudymenu.com', 'https://www.qr.cloudymenu.com',  'https://www.cloudymenu.com', 'https://cloudymenu.com'], // Allow both domains
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+//     credentials: true, // Allow credentials (if needed)
+// }));
 
 // app.use(cors());
 app.use(bodyParser.json());
