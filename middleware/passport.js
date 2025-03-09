5ae20e065c719b411864afdcffb8b7a803c05949
tuseefahmed786 <86749819+tuseefahmed786@users.noreply.github.com>
2025-03-09 06:57:19 +0500

    Update passport.js

--- middleware/passport.js
+++ middleware/passport.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3002/google/callback"
+    callbackURL: "https://menuserver-eight.vercel.app/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const email = profile.emails[0].value;
